f0d0d875000cf773a29457f690921bae62339e0c
Thomas Izycki <thomas.izycki2@hs-augsburg.de>
2024-05-26 14:04:29 +0200

    Delay the spawning of the Crazyflies

--- launch/spawn_in_gazebo.launch.py
+++ launch/spawn_in_gazebo.launch.py
@@ -1,7 +1,7 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, TimerAction
 from launch.substitutions import LaunchConfiguration
 from launch.actions import OpaqueFunction
 
@@ -17,25 +17,30 @@ def launch_setup(context, *args, **kwargs):
     robot_description_topic_name = "/" + entity_name + "_robot_description"
     robot_state_publisher_name= entity_name + "_robot_state_publisher"
 
-    # Spawn ROBOT in Gazebo
-    spawn_robot = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        name='crazyflie_spawn_entity',
-        output='screen',
-        emulate_tty=True,
-        arguments=['-entity',
-                   entity_name,
-                   '-x', str(x_initial),
-                   '-y', str(y_initial),
-                   '-z', str(z_initial),
-                   '-R', str(roll_initial),
-                   '-P', str(pitch_initial),
-                   '-Y', str(yaw_initial),
-                   '-topic', robot_description_topic_name
-                   ],
-        remappings=[("/robot_state_publisher", robot_state_publisher_name)
-                    ]
+    # Spawn ROBOT in Gazebo with delay
+    spawn_robot = TimerAction(
+        period=5.0,  # Adjust this delay as necessary
+        actions=[
+            Node(
+                package='gazebo_ros',
+                executable='spawn_entity.py',
+                name='crazyflie_spawn_entity',
+                output='screen',
+                emulate_tty=True,
+                arguments=['-entity',
+                           entity_name,
+                           '-x', str(x_initial),
+                           '-y', str(y_initial),
+                           '-z', str(z_initial),
+                           '-R', str(roll_initial),
+                           '-P', str(pitch_initial),
+                           '-Y', str(yaw_initial),
+                           '-topic', robot_description_topic_name
+                           ],
+                remappings=[("/robot_state_publisher", robot_state_publisher_name)
+                            ]
+            )
+        ]
     )
 
     return [spawn_robot]
